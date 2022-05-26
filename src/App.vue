49a39a8b2b9e7f6dae3b65a5ffe0b3da822923c6
maniksheak <mdmanik081498@gmail.com>
2022-05-26 11:45:09 +0600

    Completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="contaier">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="NewQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to Delete It!</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuoteView.vue";
+import Header from "./components/HeaderView.vue";
+
 
 export default {
-  name: 'App',
+  data: function () {
+    return {
+      quotes: ["Just a Quote to See Something"],
+      maxQuotes: 10,
+    };
+  },
+  methods: {
+    NewQuote(quote) {
+      if(this.quotes.length >= this.maxQuotes) {
+        return alert('please delete Quotes first');
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1)
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/QuoteView.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="col-sm-6 col-md-4 col-lg-3">
+       <div class="panel panel-default">
+           <div class="panel-body quote">
+               <slot></slot>
+           </div>
+       </div>
+
+   </div>
+</template>
+
+<script>
+  
+</script>
+
+<style> 
+    .panel-body {
+        font-family: 'Arizania', cursive;
+        font-size:20px;
+        color: #554f4f;
+        background: #ccc;
+        text-align: center;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
+</style>
